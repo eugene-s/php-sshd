f1e84115eb1244376acbb7cc286afdae6b45e68f
Eugene Savchenko <savchenko.eo@gmail.com>
2016-10-25 15:35:25 +0300

    Added php docker files

---
+++ php-7.0-alpine/rootfs/usr/local/bin/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/ash
+
+# Generate Host keys, if required
+if ! ls /etc/ssh/host_keys/ssh_host_* 1> /dev/null 2>&1; then
+    ssh-keygen -A
+    mv /etc/ssh/ssh_host_* /etc/ssh/host_keys/
+
+    chmod 600 -R /etc/ssh/host_keys/*
+    chmod 700 /etc/ssh/host_keys/
+fi
+
+# do not detach (-D), log to stderr (-e), passthrough other arguments
+exec /usr/sbin/sshd -D -e "$@"
